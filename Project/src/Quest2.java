daf34140acf60ab6e0e83e2c2dd2663171e92197
Sami Hindi <lolipop66611@gmail.com>
2022-01-26 10:00:58 +0100

    Ver. 0.5 Bug fix

--- Project/src/Quest2.java
+++ Project/src/Quest2.java
@@ -4,16 +4,19 @@ public class Quest2 {
 
     public static int quest() {
         System.out.println("This is the Guess the Number correct, the Name is self explanatory.");
-        System.out.println("The reward for this Quest is 20 Gems");
+        System.out.println("The reward for this Quest is 20 Gems.");
+        System.out.println("You will lose 1 Gem if you get it wrong.");
+        System.out.println("The Number is between 1 and 10.");
         System.out.println("Your Guess: ");
         int guess = readInt();
         if(isCorrect(guess)) {
             System.out.println("Correct!");
             gems += 20;
-            System.out.println("20 Gems were added your Account, Good Job!");
+            System.out.println("20 Gems were added to your Account, Good Job!");
         } else {
             System.out.println("Sadly, that's wrong!");
-            System.out.println("0 Gems were added your Account, Good Luck next Time!");
+            gems -= 1;
+            System.out.println("1 Gem was removed from your Account, for guessing wrong.");
         }
         return gems;
     }
@@ -26,6 +29,8 @@ public class Quest2 {
         return Integer.parseInt(Code.scanner.nextLine());
     }
     public static boolean isCorrect(int num) {
-        return num == randomInt();
+        int random = randomInt();
+        System.out.println(random);
+        return num == random;
     }
 }
