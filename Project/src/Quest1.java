1cc96647335d5fcab57b83f8b35dd56dbf8ffd36
Sami Hindi <lolipop66611@gmail.com>
2022-01-30 16:57:06 +0100

    Ver. 0.5 Big Bug Fix

--- Project/src/Quest1.java
+++ Project/src/Quest1.java
@@ -7,36 +7,30 @@ public class Quest1 {
     * Measures Time between Start and End of while loop (Dunk)
     * */
     public static int quest() throws InterruptedException {
-        int counter = 0;
+        Game game = new Game();
         int amountOfEnters = 0;
-        long start = System.nanoTime();
         System.out.println("This Quest is about catching a Pokémon.");
         System.out.println("Press [Enter] as many times as you can, in a matter of 5 Seconds"+
                 ", to catch the Pokémon.");
 
-        Dunk:
-        while (counter <= 2) {
-            counter++;
+        double s = 5000;
+
+        long start = System.currentTimeMillis();
+        while(System.currentTimeMillis() < start+s){
+            System.out.print("[Enter]");
             if(Code.scanner.hasNextLine()) {
                 amountOfEnters++;
+                Code.scanner.nextLine();
             }
         }
 
-        long end = System.nanoTime();
-        long elapsedTime = end - start;
-        double s = 500000000D;
-        Game game = new Game();
-
-        if(amountOfEnters > 1 && elapsedTime < s ) {
+        if(amountOfEnters > 10) {
             System.out.println("You've successfully pressed [Enter] " +
-                    amountOfEnters + " in under 5 Seconds.");
+                    amountOfEnters + " times, in under 5 Seconds.");
             gems = amountOfEnters / 2;
             System.out.println("You received "+gems+" Gems from this Quest.");
             gems += game.getGemCount();
             return gems;
-        } else if (elapsedTime > s){
-            System.out.println("You weren't fast enough.");
-            return 0;
         } else {
             return 0;
         }
