c4591f9bced9b6c3a22730e179bb92064c515de0
Sami Hindi <lolipop66611@gmail.com>
2022-01-30 19:51:37 +0100

    Ver 0.83 -

--- Project/src/Guide.java
+++ Project/src/Guide.java
@@ -3,6 +3,7 @@ public class Guide {
     // To be made in the near Future...
     public static void printGuide() {
         System.out.println("Guide:");
-        System.out.println("one that leads or directs another's way");
+        System.out.println("The defintion of Guide is to lead or direct another one's way.");
+        System.out.println();
     }
 }
